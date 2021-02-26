97d4a6ac1cc4fb0fd2cf6f2b09de5589e2d1ce50
Fa ZHANG <pure.zf@hotmail.com>
2021-02-26 10:51:40 -0800

    Kata 1 - Sum the Largest Numbers

--- sumLargest.js
+++ sumLargest.js
@@ -22,5 +22,3 @@ const sumLargestNumbers = function(data) {
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
-console.log("make some changes")
-console.log("make some changes")
