39834e7cd2ae0a54ea4ca6f7da364c6848629e87
Fa ZHANG <pure.zf@hotmail.com>
2021-02-26 13:50:42 -0800

    Kata 12 - The Great Codeville Bake-off

---
+++ bakery.js
@@ -0,0 +1,51 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  // Code here!
+  let bakeryStock = bakeryA.concat(bakeryB);
+  let ingredientNeeded = 0;
+  for (recipe of recipes){
+    for (ingredient of recipe.ingredients){
+      bakeryStock.includes(ingredient) ? ingredientNeeded +=1 : ingredientNeeded +=0;
+      if (ingredientNeeded == recipe.ingredients.length){
+        return recipe.name
+      }
+    }
+  }
+}
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+    {
+        name: 'Coconut Sponge Cake',
+        ingredients: ['coconut', 'cake base']
+    },
+    {
+        name: 'Persian Cheesecake',
+        ingredients: ['saffron', 'cream cheese']
+    },
+    {
+        name: 'Custard Surprise',
+        ingredients: ['custard', 'ground beef']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
+    }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
