60965edf17705110c4c0e3ef7a311096fef96395
Fa ZHANG <pure.zf@hotmail.com>
2021-02-26 22:16:08 -0800

    Kata 18 - Square Code

---
+++ squareCode.js
@@ -0,0 +1,51 @@
+const squareCode = function(message) {
+  // Put your solution here
+  let newText = ""
+  let finalText = ""
+  let text = message.split(" ").join("")
+  let a = Math.ceil(Math.sqrt(text.length))
+  let k = 0;
+  
+  // convert to square
+  
+  for (i = 0; i < a; i ++){
+    if((i+1)*a >= text.length){
+      newText += text.slice(i*a, text.length) 
+    }
+    else{
+    newText += text.slice(i*a, (i+1)*a)+ "\n"
+    }
+  }
+
+  // find # of rows:
+  const rows = newText.match(/^/gm).length
+
+  // encoding
+  for (i = 0; i < a; i++){
+    for (j = 0; j < rows; j++){
+      if(j == rows -1){
+        if(newText[j*a+j+k]){
+      finalText += newText[j*a+j+k] + " "
+        }
+        else{
+          finalText += " "
+        }
+      }
+      else{
+      finalText += newText[j*a+j+k]; 
+      }
+    }
+    k+=1
+  }
+  return finalText
+
+}
+
+
+
+
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots")); 
